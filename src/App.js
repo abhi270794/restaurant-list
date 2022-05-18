ac7c1d6c46a60b1c8533aedb4079713028514822
ABHISHEK SONI <86410022+abhi270794@users.noreply.github.com>
2022-05-18 13:36:53 +0530

    done

--- src/App.js
+++ src/App.js
@@ -40,10 +40,8 @@ useEffect(()=>{
            <div class="mapouter">
            <div class="gmap_canvas">
            <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=University of Oxford&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
- 
            </div>
-
-           </div>
+</div>
            </div>
         )
          })
